9ba33f03b95347f270a03c1080cf35bf81b322f7
Leonardo <chermauts@hotmail.com>
2026-02-04 17:48:09 -0300

    refactor: include autoinvoke function in popup script

--- src/modules/popup/popup.js
+++ src/modules/popup/popup.js
@@ -1,40 +1,40 @@
-const { storage, getElementById } = require("../../utils/utils");
+(() => {
+  const elements = {
+    facebook: Utils.getElementById("facebook"),
+    instagram: Utils.getElementById("instagram"),
+  };
 
-const elements = {
-  facebook: getElementById("facebook"),
-  instagram: getElementById("instagram"),
-};
+  const syncCheckboxesWithState = (config) => {
+    if (!elements.facebook || !elements.instagram) return;
 
-const syncCheckboxesWithState = (config) => {
-  if (!elements.facebook || !elements.instagram) return;
+    elements.facebook.checked = config.facebookStoriesEnabled;
+    elements.instagram.checked = config.instagramStoriesEnabled;
+  };
 
-  elements.facebook.checked = config.facebookStoriesEnabled;
-  elements.instagram.checked = config.instagramStoriesEnabled;
-};
-
-const updatePreferencesFromUI = async () =>
-  await storage.set({
-    facebookStoriesEnabled: elements.facebook?.checked,
-    instagramStoriesEnabled: elements.instagram?.checked,
-  });
-
-const init = async () => {
-  const config = await storage.get();
-  syncCheckboxesWithState(config);
-
-  elements.facebook?.addEventListener("change", updatePreferencesFromUI);
-  elements.instagram?.addEventListener("change", updatePreferencesFromUI);
-};
-
-init();
-
-if (typeof module !== "undefined") {
-  module.exports = {
-    getCurrentConfig: () => ({
+  const updatePreferencesFromUI = async () =>
+    await Utils.storage.set({
       facebookStoriesEnabled: elements.facebook?.checked,
       instagramStoriesEnabled: elements.instagram?.checked,
-    }),
-    syncCheckboxesWithState,
-    fetchUserPreferences: storage.get,
+    });
+
+  const init = async () => {
+    const config = await Utils.storage.get();
+    syncCheckboxesWithState(config);
+
+    elements.facebook?.addEventListener("change", updatePreferencesFromUI);
+    elements.instagram?.addEventListener("change", updatePreferencesFromUI);
   };
-}
+
+  init();
+
+  if (typeof module !== "undefined") {
+    module.exports = {
+      getCurrentConfig: () => ({
+        facebookStoriesEnabled: elements.facebook?.checked,
+        instagramStoriesEnabled: elements.instagram?.checked,
+      }),
+      syncCheckboxesWithState,
+      fetchUserPreferences: Utils.storage.get,
+    };
+  }
+})();
